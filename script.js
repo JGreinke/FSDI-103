b74297b07919625f4553cc543597d79b5053c11a
jazshon <89039050+jazshon@users.noreply.github.com>
2021-12-21 18:36:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//header section
+var nameFirst = ("Jasmine");
+var nameLast = ("Greinke");
+var todaysDate = new Date("December 16 2021"); 
+var moveInDate = ("February 20 2021"); 
+var currentMonthlyIncome = (7000); 
+//address
+var currentAddress = ("123 Seasame Street, Elmo, Ohio 12345"); 
+//references
+var referenceNameOne = ("Joe Smith"); 
+var referencePhoneOne = ("123-456-7890"); 
+var yearsKnownOne = (2); 
+var emailOne = ("joesmith@gmail.com"); 
+var referenceNameTwo = ("Billy Bob"); 
+var referencePhoneTwo = ("098-765-4321"); 
+var yearsKnownTwo = (7); 
+var emailTwo = ("billy-bob@gmail.com"); 
+//realtor info
+var realtorName = ("Betty Boop"); 
+var realtorContactDate = ("October 29 2021"); 
+var locationApplication = ("456 Brown Road, Bookersville, Ohio 78910"); 
+var completeApplication = (true); 
+var applicantAnnualIncome = (currentMonthlyIncome*12); 
+var monthlyRent = (1500); 
